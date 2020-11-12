8dd9a32c1cd93c15b4ad2efbba08021774116e0f
LvQiang <lq15512728505@163.com>
2020-11-12 19:23:28 +0800

    init project

---
+++ JLLog/Classes/JlLog.h
@@ -0,0 +1,18 @@
+//
+//  JlLog.h
+//  JLLog
+//
+//  Created by John Lyn on 2020/11/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JlLog : NSObject
+
+- (void)log123;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JLLog/Classes/JlLog.m
@@ -0,0 +1,15 @@
+//
+//  JlLog.m
+//  JLLog
+//
+//  Created by John Lyn on 2020/11/12.
+//
+
+#import "JlLog.h"
+
+@implementation JlLog
+- (void)log123
+{
+    NSLog(@"123");
+}
+@end
